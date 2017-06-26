40c7e4ce9465a0a1bf7e98a55f84b9f351bfdf4d
Maximilian Schwarzmüller <mblacky0@gmail.com>
2017-06-26 16:36:43 +0200

    finished setup

--- src/app/topics.service.ts
+++ src/app/topics.service.ts
@@ -12,19 +12,19 @@ export class TopicsService {
   constructor(private http: Http) { }
 
   fetchData (): Observable<TopicsDataPoint[]> {
-    // return this.http.get('')
-    //   .map((result: Response) => result.json());
-    return Observable.create((observer: Observer<TopicsDataPoint[]>) => {
-      setTimeout(() => {
-        observer.next([
-          { title: 'Vue.js', value: 30 },
-          { title: 'Full Project', value: 30 },
-          { title: 'Electron', value: 30 },
-          { title: 'Angular', value: 30 },
-          { title: 'Laravel', value: 30 }
-        ])
-      }, 2000);
-    })
+    return this.http.get('https://5kr4p4sctf.execute-api.us-east-1.amazonaws.com/dev/simple-test')
+      .map((result: Response) => result.json());
+    // return Observable.create((observer: Observer<TopicsDataPoint[]>) => {
+    //   setTimeout(() => {
+    //     observer.next([
+    //       { title: 'Vue.js', value: 30 },
+    //       { title: 'Full Project', value: 30 },
+    //       { title: 'Electron', value: 30 },
+    //       { title: 'Angular', value: 30 },
+    //       { title: 'Laravel', value: 30 }
+    //     ])
+    //   }, 2000);
+    // })
   }
 
 }
